258f848b1c2e97a980c100aaf34b48af61798ca7
Alpaca Zen <alpacazendev@gmail.com>
2025-09-13 08:02:03 -0500

    Finalizado e14: Modelo de código estructurado. Referencia para los demás ejercicios.

--- e14_suma-de-digitos.js
+++ e14_suma-de-digitos.js
@@ -3,23 +3,43 @@
  * 14. Leer dos números enteros de dos dígitos y determinar a cuánto es igual
  * la suma de todos los dígitos.
  */
+// ! Modelo de código estructurado para ejercicios TS.
+/**
+ * 📖 Justificación
+ * Validación: función para garantizar que los datos de entrada cumplan el enunciado.
+ * Lógica: función que resuelve el problema y retorna un resultado (reutilizable).
+ * Presentación: función main que gestiona la interacción con el usuario (mensajes, salidas).
+ * Aislamiento: uso de IIFE para no contaminar el espacio global.
+ * Esto es más que resolver un problema puntual:
+ * 👉 es un modelo metodológico que separa qué hace el programa (lógica) de cómo se muestra el resultado (presentación).
+ */
 (() => {
+    // *Validación:
     function evaluateNumber(num) {
         if (!Number.isInteger(num))
             return false;
         return (num >= 10 && num <= 99);
     }
+    // *Lógica:
     function addDigits(numA, numB) {
         if (!evaluateNumber(numA) || !evaluateNumber(numB)) {
-            console.log(`Ingrese sólo enteros positivos de dos dígitos!`);
+            return null;
+        }
+        const numATens = Math.trunc(numA / 10);
+        const numAUnits = numA % 10;
+        const numBTens = Math.trunc(numB / 10);
+        const numBUnits = numB % 10;
+        return (numATens + numAUnits + numBTens + numBUnits);
+    }
+    // *Presentación:
+    function main() {
+        const result = addDigits(82, 42);
+        if (result === null) {
+            console.log(`Ingrese sólo positivos de dos dígitos`);
         }
         else {
-            const a1 = Math.trunc(numA / 10);
-            const a2 = numA % 10;
-            const b1 = Math.trunc(numB / 10);
-            const b2 = numB % 10;
-            console.log(`Suma de todos los dígitos: ${a1 + a2 + b1 + b2}`);
+            console.log(`Suma de todos los dígitos de ambos números: ${result}`);
         }
     }
-    addDigits(82, 42);
+    main();
 })();
