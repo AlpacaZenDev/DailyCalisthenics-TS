41f29ec7a942203416be2fd143c7d0635a0a225f
Alpaca Zen <alpacazendev@gmail.com>
2025-09-22 05:34:40 -0500

    Ending function ValidateNumber()

---
+++ e16_compare-digits.js
@@ -0,0 +1,17 @@
+"use strict";
+/**
+ * 16. Leer un número entero de tres dígitos y determinar si al menos dos de
+ * sus tres dígitos son iguales.
+ */
+(() => {
+    // Validation
+    function ValidateNumber(num) {
+        num = Math.abs(num);
+        if (!Number.isInteger(num))
+            return false;
+        return (num >= 100 && num <= 999);
+    }
+    // console.log(ValidateNumber(999));
+    // Logic
+    // Display
+})();
