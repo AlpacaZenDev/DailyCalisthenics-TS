46f8aa16e2260807189352b89b3f5376b38e0628
Alpaca Zen <alpacazendev@gmail.com>
2025-09-22 06:01:54 -0500

    ending CompareDigits() -> return digits

--- e16_compare-digits.js
+++ e16_compare-digits.js
@@ -4,7 +4,7 @@
  * sus tres dígitos son iguales.
  */
 (() => {
-    // Validation
+    //* Validation
     function ValidateNumber(num) {
         num = Math.abs(num);
         if (!Number.isInteger(num))
@@ -12,6 +12,19 @@
         return (num >= 100 && num <= 999);
     }
     // console.log(ValidateNumber(999));
-    // Logic
-    // Display
+    // todo: ¿Cómo valido mediante debug? -> Prácticar ambos métodos
+    //* Logic
+    function CompareDigits(num) {
+        if (!ValidateNumber(num))
+            return null;
+        num = Math.abs(num);
+        const numHundred = Math.trunc(num / 100);
+        const numTen = Math.trunc(num / 10) % 10;
+        const numUnit = num % 10;
+        // console.log(numHundred);
+        // console.log(numTen);
+        // console.log(numUnit);
+    }
+    CompareDigits(456); // Evaluar parcialmente;
+    //* Display
 })();
--- e16_compare-digits.ts
+++ e16_compare-digits.ts
@@ -6,7 +6,7 @@
 
 (()=>{
 
-    // Validation
+    //* Validation
     function ValidateNumber(num:number) {
         num = Math.abs(num);
         if (!Number.isInteger(num)) return false;
@@ -14,14 +14,26 @@
     }
     
     // console.log(ValidateNumber(999));
+    // todo: ¿Cómo valido mediante debug? -> Prácticar ambos métodos
     
+    //* Logic
+    function CompareDigits(num:number): any {
+        if (!ValidateNumber(num)) return null;
+        
+        num = Math.abs(num);
+        const numHundred: number = Math.trunc(num / 100);
+        const numTen: number = Math.trunc(num / 10) % 10;
+        const numUnit: number = num % 10;
+        // console.log(numHundred);
+        // console.log(numTen);
+        // console.log(numUnit);
+        
+
+    }
+    CompareDigits(456) // Evaluar parcialmente;
 
 
-    // Logic
-    
-
-
-    // Display
+    //* Display
 
 
 
